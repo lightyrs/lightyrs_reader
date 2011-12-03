78cfc1c87414cdc6eb5e499cb99f224d919efb08
lightyrs <harris@harrisnovick.com>
2011-12-03 17:49:35 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LightyrsReader::Application.config.secret_token = '70623366b938c40082926e1ab5f1984b1dd2c508d0a699fd67c1b58ad849889ce489ef21cf81c366591d7fafb4143f76aad02452c65307345ee0c54c787179e8'
